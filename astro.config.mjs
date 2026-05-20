9631606b4ca21b8bd2244bd1e3c8ab3aeaee10da
Natalia <alamo.natalia@gmail.com>
2026-05-20 20:03:23 +0200

    probando deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://alamonatalia.github.io/',
-  base: '/stefs-bakery',
+  base: '/stefs-bakery/',
   vite: {
     plugins: [tailwindcss()]
   }
