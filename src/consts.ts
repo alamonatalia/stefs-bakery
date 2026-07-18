5271477cf24d8816cb76b7dcb60e22cfc06327d2
Natalia <alamo.natalia@gmail.com>
2026-07-18 13:39:56 +0200

    ajuste de meta etiquetas de seo

---
+++ src/consts.ts
@@ -0,0 +1,8 @@
+export const SITE = {
+  title: "Stef's Bakery",
+  description: "Pastelería artesanal Peruana en Málaga, España. Alfajores de lúcuma y matcha.",
+  url: "https://midominio.com",
+  author: "Stef's Bakery",
+  locale: "es_ES",
+  themeColor: "#2A1F14",
+};
